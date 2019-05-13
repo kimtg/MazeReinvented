baf8975335db3d1feb85d24b7dec86b6cdda2560
KIM Taegyoon <steloflute@gmail.com>
2019-05-13 23:40:34 +0900

    boundary check

--- unitmain.pas
+++ unitmain.pas
@@ -18,7 +18,7 @@ type
     MenuGenerate: TMenuItem;
     MenuOptions: TMenuItem;
     MenuSolve: TMenuItem;
-    procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    procedure FormKeyDown(Sender: TObject; var Key: word; Shift: TShiftState);
     procedure MenuGenerateClick(Sender: TObject);
     procedure MenuOptionsClick(Sender: TObject);
     procedure MenuSolveClick(Sender: TObject);
@@ -29,7 +29,8 @@ type
   end;
 
 type
-  tmat = (clear, wall, current, visited, deadend);
+  tmat = (Clear, wall, current, visited, deadend);
+
 var
   FormMain: TFormMain;
   wallsize: integer = 10;
@@ -39,7 +40,7 @@ var
   ColorWall: tcolor = clGray;
   ColorCur: tcolor = clGreen;
   ColorDeadEnd: tcolor = clRed;
-  finished: boolean = false;
+  finished: boolean = False;
 
 implementation
 
@@ -54,22 +55,24 @@ procedure DrawMat();
 var
   x, y, x2, y2: integer;
 begin
-  formmain.canvas.Clear;
-  formmain.canvas.Pen.Width := wallsize div 4;
-  formmain.canvas.pen.color := colorwall;
+  FormMain.canvas.Clear;
+  FormMain.canvas.Pen.Width := wallsize div 4;
+  FormMain.canvas.pen.color := colorwall;
   for y := 0 to maxy div 2 do
     for x := 0 to maxx div 2 do
     begin
       x2 := x * 2;
       y2 := y * 2;
       //FormMain.canvas.Pixels[x2*wallsize,y2*wallsize] := clGreen;
-      if mat[y2,x2+1] = wall then // horizontal
+      if mat[y2, x2 + 1] = wall then // horizontal
       begin
-        FormMain.canvas.Line(x2*wallsize, y2*wallsize, (x2+2)*wallsize, y2*wallsize);
+        FormMain.canvas.Line(x2 * wallsize, y2 * wallsize, (x2 + 2) *
+          wallsize, y2 * wallsize);
       end;
-      if mat[y2+1,x2] = wall then // vertical
+      if mat[y2 + 1, x2] = wall then // vertical
       begin
-        FormMain.canvas.Line(x2*wallsize, y2*wallsize, x2*wallsize, (y2+2)*wallsize);
+        FormMain.canvas.Line(x2 * wallsize, y2 * wallsize, x2 * wallsize,
+          (y2 + 2) * wallsize);
       end;
     end;
 
@@ -79,11 +82,11 @@ begin
     x := 1;
     while x < maxx do
     begin
-      if mat[y,x] = visited then
-        FormMain.canvas.Pixels[x*wallsize, y*wallsize] := ColorDeadEnd;
-      inc(x,2);
+      if mat[y, x] = visited then
+        FormMain.canvas.Pixels[x * wallsize, y * wallsize] := ColorDeadEnd;
+      Inc(x, 2);
     end;
-    inc(y,2);
+    Inc(y, 2);
   end;
 end;
 
@@ -92,7 +95,7 @@ var
   x, y, dir: integer;
   HasWork: boolean;
 begin
-  finished := false;
+  finished := False;
   maxy := FormMain.ClientHeight div wallsize div 2 * 2;
   maxx := FormMain.ClientWidth div wallsize div 2 * 2;
   setlength(mat, maxy + 2, maxx + 2);
@@ -100,7 +103,7 @@ begin
   for y := 0 to maxy do
     for x := 0 to maxx do
     begin
-      mat[y, x] := clear;
+      mat[y, x] := Clear;
     end;
 
   for x := 0 to maxx do
@@ -115,21 +118,21 @@ begin
     mat[y, maxx] := wall;
   end;
 
-  mat[1, 0] := clear; // start
-  mat[maxy - 1, maxx] := clear; //end
+  mat[1, 0] := Clear; // start
+  mat[maxy - 1, maxx] := Clear; //end
 
   repeat
-    HasWork := false;
+    HasWork := False;
     y := 0;
     while y <= maxy do
     begin
       x := 0;
       while x <= maxx do
       begin
-        if mat[y,x] <> wall then
+        if mat[y, x] <> wall then
         begin
-          HasWork := true;
-          inc(x, 2);
+          HasWork := True;
+          Inc(x, 2);
           continue;
         end;
 
@@ -153,28 +156,28 @@ begin
               mat[y + 1, x] := wall;
               mat[y + 2, x] := wall;
             end;
-          3:
+          else
             if (y - 2 >= 0) and (mat[y - 2, x] <> wall) then
             begin
               mat[y - 1, x] := wall;
               mat[y - 2, x] := wall;
             end;
         end;
-        inc(x, 2);
+        Inc(x, 2);
       end;
-      inc(y, 2);
+      Inc(y, 2);
     end;
   until not HasWork;
 
   DrawMat;
   curx := 1;
   cury := 1;
+  mat[cury, curx] := visited;
   canvas.Pen.Color := ColorCur;
-  canvas.line(0*wallsize, 1*wallsize, curx*wallsize, cury*wallsize);
+  canvas.line(0 * wallsize, 1 * wallsize, curx * wallsize, cury * wallsize);
 end;
 
-procedure TFormMain.FormKeyDown(Sender: TObject; var Key: Word;
-  Shift: TShiftState);
+procedure TFormMain.FormKeyDown(Sender: TObject; var Key: word; Shift: TShiftState);
 var
   oldx, oldy: integer;
 begin
@@ -182,40 +185,47 @@ begin
   oldy := cury;
   case key of
     vk_up:
-      if mat[cury-1,curx] <> wall then dec(cury, 2);
+      if mat[cury - 1, curx] <> wall then
+        Dec(cury, 2);
     vk_down:
-      if mat[cury+1,curx] <> wall then inc(cury, 2);
+      if mat[cury + 1, curx] <> wall then
+        Inc(cury, 2);
     vk_left:
-      if mat[cury,curx-1] <> wall then dec(curx, 2);
+      if (curx > 1) and (mat[cury, curx - 1] <> wall) then
+        Dec(curx, 2);
     vk_right:
-      if mat[cury,curx+1] <> wall then inc(curx, 2);
+      if mat[cury, curx + 1] <> wall then
+        Inc(curx, 2);
   end;
-  if mat[cury,curx] <> visited then
+  if mat[cury, curx] <> visited then
   begin
-    mat[cury,curx] := visited;
-    canvas.Pen.Color := ColorCur
+    mat[cury, curx] := visited;
+    canvas.Pen.Color := ColorCur;
   end
   else
   begin
-    mat[oldy,oldx] := clear;
+    mat[oldy, oldx] := Clear;
     canvas.Pen.Color := ColorDeadEnd;
   end;
-  canvas.Line(oldx*wallsize, oldy*wallsize, curx*wallsize, cury*wallsize);
+  canvas.Line(oldx * wallsize, oldy * wallsize, curx * wallsize, cury * wallsize);
   if not finished and (curx = maxx - 1) and (cury = maxy - 1) then
   begin
-    finished := true;
+    finished := True;
     ShowMessage('Congratulations!');
   end;
 end;
 
 procedure TFormMain.MenuOptionsClick(Sender: TObject);
 begin
-  FormOptions.show;
+  FormOptions.Show;
 end;
 
 function MatBlocked(x, y: integer): integer;
 begin
-  if mat[y,x] = clear then MatBlocked := 0 else MatBlocked := 1;
+  if mat[y, x] = Clear then
+    Result := 0
+  else
+    Result := 1;
 end;
 
 procedure TFormMain.MenuSolveClick(Sender: TObject);
@@ -224,25 +234,30 @@ var
   HasWork: boolean;
 begin
   repeat
-    HasWork := false;
+    HasWork := False;
     y := 1;
     while y < maxy do
     begin
       x := 1;
       while x < maxx do
       begin
-        if (mat[y,x] <> visited) and (MatBlocked(x+1,y) + MatBlocked(x-1,y) + MatBlocked(x,y+1) + MatBlocked(x,y-1) >= 3) then
+        if (mat[y, x] <> visited) and (MatBlocked(x + 1, y) +
+          MatBlocked(x - 1, y) + MatBlocked(x, y + 1) + MatBlocked(x, y - 1) >= 3) then
         begin
-          mat[y,x]:=visited;
-          if mat[y-1,x] = clear then mat[y-1,x]:=deadend;
-          if mat[y+1,x] = clear then mat[y+1,x]:=deadend;
-          if mat[y,x-1] = clear then mat[y,x-1]:=deadend;
-          if mat[y,x+1] = clear then mat[y,x+1]:=deadend;
-          HasWork := true;
+          mat[y, x] := visited;
+          if mat[y - 1, x] = Clear then
+            mat[y - 1, x] := deadend;
+          if mat[y + 1, x] = Clear then
+            mat[y + 1, x] := deadend;
+          if mat[y, x - 1] = Clear then
+            mat[y, x - 1] := deadend;
+          if mat[y, x + 1] = Clear then
+            mat[y, x + 1] := deadend;
+          HasWork := True;
         end;
-        inc(x, 2);
+        Inc(x, 2);
       end;
-      inc(y, 2);
+      Inc(y, 2);
     end;
   until not HasWork;
   DrawMat;
@@ -251,4 +266,3 @@ end;
 begin
   randomize;
 end.
-
